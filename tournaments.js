e5dfa8e2bea278ae078bb75fe99b05dba6eb8158
votre_nom_utilisateur <votre_email_utilisateur>
2024-04-19 16:37:14 +0200

    Initial commit

--- tournaments.js
+++ tournaments.js
@@ -1,33 +1,27 @@
 // tournaments.js
 
-// Fonction pour récupérer et afficher les données du JSON
+// Fonction pour afficher les tournois sur la carte
 function displayTournaments() {
-    fetch('chemin/vers/votre/fichier.json')
-        .then(response => response.json())
-        .then(data => {
-            var atp250Data = data.atp250;
-            var mymap = L.map('map-container').setView([46.6031, 1.7192], 5);
+    // Définition de la carte et de son centre
+    var mymap = L.map('map').setView([46.6031, 1.7192], 5);
 
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '© OpenStreetMap contributors'
-            }).addTo(mymap);
+    // Ajout de la couche de tuiles OpenStreetMap à la carte
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
+    }).addTo(mymap);
 
-            atp250Data.forEach(tournament => {
-                var coordinates = tournament.coordinates;
-                var date = tournament.date_debut + " - " + tournament.date_fin;
-                L.marker(coordinates).addTo(mymap).bindPopup('ATP 250 Tournoi - Dates : ' + date);
-            });
 
-            var masters1000Coordinates = data.masters1000.coordinates;
-            var masters1000Date = data.masters1000.date;
-            var masters1000 = L.marker(masters1000Coordinates).addTo(mymap).bindPopup('Masters 1000 - Rolex Paris Masters<br>Dates : ' + masters1000Date);
+    // Masters 1000 - Rolex Paris Masters
+    var masters1000Coordinates = [48.8398, 2.2519]; // Exemple de coordonnées (Paris, France)
+    var masters1000Date = "2024-10-28 - 2024-11-03"; // Exemple de dates
+    var masters1000 = L.marker(masters1000Coordinates).addTo(mymap).bindPopup('Masters 1000 - Rolex Paris Masters<br>Dates : ' + masters1000Date);
 
-            var grandSlamCoordinates = data.grandSlam.coordinates;
-            var grandSlamDate = data.grandSlam.date;
-            var grandSlam = L.marker(grandSlamCoordinates).addTo(mymap).bindPopup('Grand Chelem - Roland Garros<br>Dates : ' + grandSlamDate);
-        })
-        .catch(error => console.error('Erreur lors de la récupération des données JSON :', error));
+    // Grand Chelem - Roland Garros
+    var grandSlamCoordinates = [48.8566, 2.3509]; // Exemple de coordonnées (Paris, France)
+    var grandSlamDate = "2024-05-26 - 2024-06-09"; // Exemple de dates
+    var grandSlam = L.marker(grandSlamCoordinates).addTo(mymap).bindPopup('Grand Chelem - Roland Garros<br>Dates : ' + grandSlamDate);
 }
 
 // Appel de la fonction pour afficher les tournois
 displayTournaments();
+
