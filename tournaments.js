e749bbfd4e7c5438a4acf880146568ecf17ab965
votre_nom_utilisateur <votre_email_utilisateur>
2025-01-27 03:39:39 -0500

    final version

--- tournaments.js
+++ tournaments.js
@@ -12,12 +12,12 @@ function displayTournaments() {
 
 
     // Masters 1000 - Rolex Paris Masters
-    var masters1000Coordinates = [48.8398, 2.2519]; // Exemple de coordonnées (Paris, France)
+    var masters1000Coordinates = [48.8566, 2.3509]; // Exemple de coordonnées (Paris, France)
     var masters1000Date = "2024-10-28 - 2024-11-03"; // Exemple de dates
     var masters1000 = L.marker(masters1000Coordinates).addTo(mymap).bindPopup('Masters 1000 - Rolex Paris Masters<br>Dates : ' + masters1000Date);
 
     // Grand Chelem - Roland Garros
-    var grandSlamCoordinates = [48.8566, 2.3509]; // Exemple de coordonnées (Paris, France)
+    var grandSlamCoordinates = [48.8398, 2.2519]; // Exemple de coordonnées (Paris, France)
     var grandSlamDate = "2024-05-26 - 2024-06-09"; // Exemple de dates
     var grandSlam = L.marker(grandSlamCoordinates).addTo(mymap).bindPopup('Grand Chelem - Roland Garros<br>Dates : ' + grandSlamDate);
 }
